42e2175dd3905332fa2fff518785c074363b3256
Mati1432 <mateuszsliwinski9@gmail.com>
2022-11-21 14:24:57 +0100

    change ssi subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import zad1, zad2
 
-df = zad1.DecisionSystem('iris.txt', 'iris-type.txt')
+df = zad1.DecisionSystem('data/iris.txt', 'data/iris-type.txt')
 print(df.get_all_objects()[0].values)
 print(df.get_attr_name())
 print(df.get_attr_type(1))
--- zad5.py
+++ zad5.py
@@ -1,2 +1,24 @@
-def greedy_point():
-    pass
+import numpy as np
+
+BA = np.matrix(
+    [
+        [1, 1, 1, 1],
+        [0, 0, 0, 1],
+        [1, 1, 1, 1],
+        [0, 0, 1, 1],
+        [1, 1, 1, 1],
+    ]
+)
+BB = np.matrix(
+    [
+        [0, 0, 0, 1],
+        [0, 0, 1, 1],
+        [0, 1, 0, 1],
+        [0, 0, 0, 1],
+        [0, 0, 0, 1],
+    ]
+)
+
+
+def greedy_point(BA, BB):
+    measure = 0
